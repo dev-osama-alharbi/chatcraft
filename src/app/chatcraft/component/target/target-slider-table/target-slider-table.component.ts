6269f891bbbbe3bebc4c8787c402e92ac8f14eef
dev-osama-alharbi <osama.ad.001@gmail.com>
2023-12-16 22:59:28 +0300

    asd

---
+++ src/app/chatcraft/component/target/target-slider-table/target-slider-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-target-slider-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './target-slider-table.component.html',
+  styleUrl: './target-slider-table.component.css'
+})
+export class TargetSliderTableComponent {
+
+}
